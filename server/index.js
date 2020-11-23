7bb407787bf7d6d69cba710cd3d70ae1773e7d0d
jacksonet00 <jacksoneliastaylor@gmail.com>
2020-11-22 21:30:22 -0500

    implement firestore

--- server/index.js
+++ server/index.js
@@ -1,23 +1,45 @@
 const express = require('express');
 const Twit = require('twit');
-const config = require('./config');
+const secret = require('./secret');
+const firebase = require('./config/firebase');
+require('firebase/firestore');
+
+const db = firebase.firestore();
 
 const app = express();
 const port = 5000;
-const T = new Twit(config);
+const T = new Twit(secret);
 
-// search for tweeets contianing: 'banana'
+app.get('/users/:id', (req, res) => {
+	const { id } = req.params;
+
+	db.collection("users").get().then((querySnapshot) => {
+		querySnapshot.forEach((doc) => {
+			 console.log(`${doc.id} => ${doc.data()}`);
+		});
+  	});
+	res.status(200).send({});
+})
+
+app.post('/trends/:state', (req, res) => {
+	db.collection("users").add({})
+	.then(function(docRef) {
+		console.log("Document written with ID: ", docRef.id);
+	})
+	.catch(function(error) {
+		console.error("Error adding document: ", error);
+	});
+	res.status(200).send({});
+});
+
+
 app.get('/trends/NYC', (req, res) => {
 	T.get('trends/place', { id: 2459115 }, function(err, data, response) {
 		console.log(data)
 		console.log(data[0].trends[0])
 	 })
 	res.status(200).send('test');
-})
-
-app.get('/trends/eagles', (req, res) => {
-	T.get('search/tweets', { })
-})
+});
 
 app.get('/', (req, res) => {
 	res.status(200).send('Trendviz');
---
+++ server/secret.js
@@ -0,0 +1,10 @@
+const config = {
+	consumer_key: 'GezZTXwNWdjn2D667nVkZQxPO',
+	consumer_secret: 'LURuPRFnKpoWKNwXopuIN9LcR6gTKFXHfmBqmSECT4J2JryDyp',
+	access_token: '1151301266250850305-2zxDMWKhAytZafbuOpcA3fRlk7QLnw',
+	access_token_secret: '08qANgEiNdMi5DsRXuvKovmTrFlEVk2beOVWrveZWTzbU',
+	//timeout_ms: 60*1000,
+	//strictSSL: true,
+}
+
+module.exports = config;

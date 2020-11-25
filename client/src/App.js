b4a2748a18cc673841234a414901d08e87d1cf41
jacksonet00 <jacksoneliastaylor@gmail.com>
2020-11-24 19:53:32 -0500

    front end connected to trend data

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,30 @@
+import React, { useEffect, useState } from 'react';
+import firebase from './config/firebase';
+
 const App = () => {
+	const [data, setData] = useState({});
+	const [loading, setLoading] = useState(true);
+	
+	useEffect(() => {
+		const db = firebase.firestore();
+		let appData = {}
+		db.collection("trends").get().then(function(querySnapshot) {
+			querySnapshot.forEach(function(doc) {
+				appData[doc.id] = doc.data();
+			});
+			setData(appData);
+	  });
+	  setLoading(false);
+	}, []);
+
 	return (
 		<div>
 			<h1>TrendViz!</h1>
+			<p>Trend Viz Data</p>
+			<p>==============</p>
+			<pre>{JSON.stringify(data, 0, 3)}</pre>
 		</div>
-	);
+		);
 };
 
 export default App;

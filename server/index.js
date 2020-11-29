92c75c57ebf0a6b04ffdb8646e5b9494657e4301
jacksonet00 <jacksoneliastaylor@gmail.com>
2020-11-28 22:29:49 -0500

    refactor server code

--- server/index.js
+++ server/index.js
@@ -1,48 +1,37 @@
 const express = require('express');
-const Twit = require('twit');
-const { secret } = require('./secret');
 const firebase = require('./config/firebase');
-const states = require('./data/states');
-const cors = require('cors');
 require('firebase/firestore');
-
-const db = firebase.firestore();
+const Twit = require('twit');
+const cors = require('cors');
+const states = require('./data/states');
+const { secret } = require('./secret');
+const { yesterdaysDate } = require('./functions/date');
 
 const app = express();
-app.use(cors());
-const port = 5000;
+const db = firebase.firestore();
 const T = new Twit(secret);
 
-function yesterdaysDate() {
-	const yesterday = new Date((new Date()).valueOf() - 2*1000*60*60*24);
-	return yesterday.toISOString().substring(0, 10);
-}
+app.use(cors());
 
-const idList = {};
+const port = 5000;
+
 setTimeout(() => {
-	console.log('refreshing...');
+	console.log('refreshing trend data...');
 	states.forEach((stateObj) => {
-		T.get('trends/place', { id: stateObj.woeid }, (err, data, response) => {
+		T.get('trends/place', { id: stateObj.woeid, }, (err, data, response) => {
 			db.collection("trends").add({
 				state: stateObj.stateCode,
 				data,
 			});
-		}).then((docRef) => {
-			idList[stateObj.stateCode] = docRef;
-		}).catch((err) => {
-			res.send(201).status({
-				'error': true,
-				'message': err,
-			});
-		})
+		});
 	});
 	console.log('refresh completed.');
-}, (15 * 60 *1000));
+}, (15 * 60 * 1000));
 
 app.get('/tweets/:trend', (req, res) => {
 	const { trend } = req.params;
-	console.log(`fetching tweets about ${trend} since ${yesterdaysDate()}`)
-	T.get('search/tweets', { q: `${trend} since:${yesterdaysDate()}`, count: 100 }, (err, data, response) => {
+	console.log(`fetching tweets about ${trend} since ${yesterdaysDate()}`);
+	T.get('search/tweets', { q: `${trend} since:${yesterdaysDate()}`, count: 100, }, (err, data, response) => {
 		if (err) {
 			return res.status(201).send({
 				error: true,
@@ -54,10 +43,6 @@ app.get('/tweets/:trend', (req, res) => {
 			data: data.statuses,
 		});
 	});
-});
-
-app.get('/', (req, res) => {
-	res.status(200).send('Trendviz');
 });
 
 app.listen(port, () => console.log(`Server listening on port ${port}`));

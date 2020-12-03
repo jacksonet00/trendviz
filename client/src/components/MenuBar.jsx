d24f6c3d700cb8edb5b90edf35a7bb1ed08ce5a8
jacksonet00 <jacksoneliastaylor@gmail.com>
2020-12-03 10:29:42 -0500

    add key grid

--- client/src/components/MenuBar.jsx
+++ client/src/components/MenuBar.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import Chip from '@material-ui/core/Chip';
+import { Grid } from '@material-ui/core';
 
 const MenuBar = ({ data }) => {
    // TODO: display unique top trends
@@ -6,6 +8,17 @@ const MenuBar = ({ data }) => {
       <div>
          <div className="topBar" style={{backgroundColor: "black", display: "flex", justifyContent: "center", "color": "#5CDB95"}}> 
             <h1>TrendViz! <span role="img" aria-label="sheep">📈</span> </h1>
+            <Grid container spacing={1}>
+               {
+                  Object.keys(data).map((trend) => {
+                     return ( 
+                        <Grid item key={trend} >
+                           <Chip label={trend} style={{ fontWeight: 'bold', color: 'white', backgroundColor: data[trend].color }} />
+                        </Grid>
+                     );
+                  })
+               }
+            </Grid>
          </div>
       </div>
    );

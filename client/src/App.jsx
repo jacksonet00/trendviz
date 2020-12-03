b49c6b4e394c48859be7f6eb5ddefa410f5e2b94
jacksonet00 <jacksoneliastaylor@gmail.com>
2020-12-03 10:29:35 -0500

    additional data at app level

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import firebase from './config/firebase';
 import { codes } from './data/states';
+import { colors } from './data/colors';
 import Map from './components/Map';
 import SideBar from './components/SideBar';
 import MenuBar from './components/MenuBar';
@@ -8,17 +9,31 @@ import Loading from './components/Loading';
 
 const App = () => {
 	const [data, setData] = useState({});
+	const [data2, setData2] = useState({});
+	const [data3, setData3] = useState({});
+	const [mapData, setMapData] = useState({});
 	const [isLoading, setIsLoading] = useState(true);
 	const [selectedState, setSelectedState] = useState('');
 	const [selectedTrend, setSelectedTrend] = useState('');
 	
 	useEffect(() => {
 		const db = firebase.firestore();
-		let appData = {}
+		let appData = {};
+		let appData2 = {};
+		let appData3 = {};
+		let appMapData = {};
+
 		db.collection("trends").get().then(function(querySnapshot) {
 			querySnapshot.forEach(function(doc) {
 				if (doc.data().data.length > 0) {
 					appData[doc.data().state] = doc.data().data[0].trends;
+					appData3[doc.data().state] = doc.data().data[0].trends.map(t => t.name);
+					
+					const appData2Set = new Set();
+					doc.data().data[0].trends.map(t => t.name).forEach((trend) => {
+						appData2Set.add(trend);
+					});
+					appData2[doc.data().state] = appData2Set;
 				}
 			});
 			codes.forEach((stateCode) => {
@@ -27,8 +42,19 @@ const App = () => {
 					// 2. if not fill with default data
 					appData[stateCode] = [];
 				}
-			})
+			});
+			let colorCount = 0;
+			Object.entries(appData3).forEach((entry) => {
+				if (!appMapData.hasOwnProperty(entry[1][0])) {
+					appMapData[entry[1][0]] = { states: [], color: colors[colorCount] };
+					colorCount++;
+				}
+				appMapData[entry[1][0]].states.push(entry[0]);
+			});
 			setData(appData);
+			setData2(appData2);
+			setData3(appData3);
+			setMapData(appMapData);
 			setIsLoading(false);
 	  });
 	}, []);
@@ -48,7 +74,7 @@ const App = () => {
 				<div>
 					<div>
 						<MenuBar
-							data={data}
+							data={mapData}
 						/>
 						{selectedState !== '' ? <h1>Selected State: {selectedState} | Selected Trend: {selectedTrend}</h1> : <h1>No State Selected</h1>}
 					</div>
@@ -60,7 +86,8 @@ const App = () => {
 						/>
 						<div style={{backgroundColor : "#3E3C41"}}>
 							<SideBar
-								data={data}
+								dataHt={data3}
+								dataSet={data2}
 								selectedState={selectedState}
 								selectedTrend={selectedTrend}
 								onSelectTrend={(trend) => setSelectedTrend(trend)}
